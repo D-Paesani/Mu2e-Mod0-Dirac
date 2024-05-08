a14682d9f8c0ea4583e1658f45a1d5c480e3fa09
D-Paesani <58980766+D-Paesani@users.noreply.github.com>
2024-05-08 14:48:12 +0200

    cccc

--- simWaveRin/genprova.py
+++ simWaveRin/genprova.py
@@ -11,21 +11,21 @@ splineShi = ROOT.TF1("splinef", splineF, splineLim[0], splineLim[1], 0).GetMaxim
 nsam = 500
 tbase = 1.0
 ybase = 0.5
-elecnoiserms = 0.5
+elecnoiserms = 5
 twave = tbase*np.arange(0,nsam)
 
 def get1wave(): #one event per window
     
     wave = np.random.normal(scale=elecnoiserms, size=nsam)
 
-    amp = ROOT.gRandom.Uniform(100.0, 1000.0)
-    t0 = tbase*ROOT.gRandom.Uniform(-5.0,5.0)
+    amp = ROOT.gRandom.Uniform(500.0, 1000.0)
+    t0 = tbase*ROOT.gRandom.Uniform(-50.0,50.0)
     bline = ROOT.gRandom.Uniform(-50.0,50.0)
     tw = amp*np.vectorize(splineEval)(twave + t0 + 0.5*nsam*tbase - splineShi) + bline
     wave += tw
 
-    for _ in range(ROOT.gRandom.Poisson(nsam*8/(100.0*tbase))):
-        iamp = np.abs(ROOT.gRandom.Gaus(0.0, 20.0)) #should use bertr
+    for _ in range(ROOT.gRandom.Poisson(25.0)):
+        iamp = np.abs(ROOT.gRandom.Gaus(0.0, 50.0)) #should use bertr
         it0 = ROOT.gRandom.Uniform(0, tbase*nsam) #not actually
         wave += iamp*np.vectorize(splineEval)(twave+it0-splineShi)
         
